112c8b9e99f9d5138007b5b663389c4e66e1273f
Jonathan Guevara <jonathanpaulguevara@gmail.com>
2022-04-02 17:21:37 -0700

    change position of submit button

--- assets/script.js
+++ assets/script.js
@@ -10,5 +10,26 @@ function quiz() {
   } else if (document.checked) {
     score--;
   }
+  if (document.getElementById("three").checked) {
+    score++;
+  } else if (document.checked) {
+    score--;
+  }
+  if (document.getElementById("four").checked) {
+    score++;
+  } else if (document.checked) {
+    score--;
+  }
+  if (document.getElementById("five").checked) {
+    score++;
+  } else if (document.checked) {
+    score--;
+  }
+  if (document.getElementById("six").checked) {
+    score++;
+  } else if (document.checked) {
+    score--;
+  }
+  alert(score);
   console.log(score);
 }
